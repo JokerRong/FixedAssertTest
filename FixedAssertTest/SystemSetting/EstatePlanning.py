0ab7e53a82588022fbcdcba25ba5c1cd3ca3187b
joker <15651959910@163com>
2019-03-05 17:20:39 +0800

    update

--- FixedAssertTest/SystemSetting/EstatePlanning.py
+++ FixedAssertTest/SystemSetting/EstatePlanning.py
@@ -1,61 +1,57 @@
 #  -*-coding:utf-8-*-
-import sys
-sys.path.append("D:\\Fixedtest\\FixedAssertTest\\Common")
-import PubFunction
-# from Common import PubFunction
+
 import traceback
 import time
 
-logContents = "+++++++++++++++++++++++++ begin"
-PubFunction.log(logContents, "SystemSetting")
 
-try:
-    log_url = "http://fixedassets.zosv2.develop.local"
-    logContents = PubFunction.Login("SystemAdmin", "Admin", "SystemSetting", log_url)
-    PubFunction.ProgramPerform[0] += 1
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://fixedassets.zosv2.develop.local")
+time.sleep(1)
+try:                                        
+    driver.find_element_by_id("Username").clear()
 except:
-    fopen = open("D:\\Fixedtest\\FixedAssertTest\\Log\\SystemSetting\\log.txt")
-    logContents = "登录失败"
-    PubFunction.log(logContents, "SystemSetting")
-    traceback.print_exc(file=fopen)
-    traceback.print_exc()
-    fopen.close()
-    PubFunction.ProgramPerform[1] += 1
-
-
+    pass
+driver.find_element_by_id("Username").send_keys("SystemAdmin")
+driver.find_element_by_id("Password").clear()
+driver.find_element_by_id("Password").send_keys("Admin")
+driver.find_element_by_name("button").click()
+time.sleep(3)
 """
 基础参数配置
 """
-# time.sleep(2)
-num = 0
-for num in range(0,17):
+time.sleep(2)
+num = 8
+for num in range(8,17):
     if num in {9,12,13,16,17}:
         pass
     else:
         time.sleep(1)
-        PubFunction.driver.find_element_by_link_text("参数配置").click()
-        ul = PubFunction.driver.find_element_by_xpath("/html/body/main/div/div/div/section/div/div/div/div/div/ul")
+        driver.find_element_by_link_text("参数配置").click()
+        ul = driver.find_element_by_xpath("/html/body/main/div/div/div/section/div/div/div/div/div/ul")
         li_list = ul.find_elements_by_css_selector("li")
+        time.sleep(1)
         li_list[num].find_element_by_xpath("./button").click()
 
         time.sleep(0.5)
-        PubFunction.driver.find_element_by_class_name("ion-plus-round").click()
+        driver.find_element_by_class_name("ion-plus-round").click()
         time.sleep(0.5)
-        name = PubFunction.driver.find_element_by_xpath("/html/body/main/div/div/content-top/div/ul/li").text
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").click()
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").send_keys("001")
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").click()
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").send_keys( name + "1")
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[1]").click()
-        # PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[2]").click()
+        name =driver.find_element_by_xpath("/html/body/main/div/div/content-top/div/ul/li").text
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").click()
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").send_keys("001")
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").click()
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").send_keys( name + "1")
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[1]").click()
+        # driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[2]").click()
         
         time.sleep(1)
-        PubFunction.driver.find_element_by_class_name("ion-plus-round").click()
+        driver.find_element_by_class_name("ion-plus-round").click()
         time.sleep(0.5)
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").send_keys("002")
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").send_keys( name + "2")
-        PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[1]").click()
-        # PubFunction.driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[2]").click()
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[1]/div/div/input").send_keys("002")
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[2]/form/div[2]/div/div/input").send_keys( name + "2")
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[1]").click()
+        # driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/button[2]").click()
 
     
 
