1958eab5aac0b0b7f94795270748ec05376ccf88
joker <15651959910@163com>
2019-03-19 09:01:25 +0800

    Split the total process into multiple small processes

--- FixedAssertTest/AssetsPurchase/AcceptanceCheck.py
+++ FixedAssertTest/AssetsPurchase/AcceptanceCheck.py
@@ -3,7 +3,7 @@ import Base
 Base.login()
 # 进入资产验收界面
 Base.enter_estate_planing(4)
-code = "201903130010"
+code = "201903150001"
 Base.choose_one_line(code)
 
 tbody_xpath = "/html/body/main/div/div/div/section/div/div/div/div[6]/table/tbody"
@@ -21,7 +21,7 @@ for number in range (0, len(tr_list)):
 
 # 安装验收单
 filepath = "D:\\Fixedtest\\FixedAssertTest\\FixedAssertTest\\AssetsPurchase\\data\\1.jpeg"
-for number in range (6, len(tr_list)):
+for number in range (0, len(tr_list)):
     Base.time.sleep(0.5)
     tbody = Base.driver.find_element_by_xpath(tbody_xpath)
     tr_list = tbody.find_elements_by_css_selector("tr")
--- FixedAssertTest/AssetsPurchase/EstatePlaning.py
+++ FixedAssertTest/AssetsPurchase/EstatePlaning.py
@@ -34,45 +34,8 @@ while record_below:
 
 
 """
-提交新增的资产规划单
+ 提交新增的资产规划单
 """
 Base.choose_one_line( code)
 Base.click_top_button(3)
 
-
-"""
-进入资产购置-资产申购界面，通过规划调入新增申购单
-"""
-Base.enter_estate_planing(1)
-Base.click_top_button(1)
-order_number = Base.transfer_planing_bill(code , False, 6)
-
-"""
-提交新增的资产申购单
-"""
-Base.choose_one_line(code)
-Base.click_top_button(6)
-
-
-"""
-进入资产购置界面，新增资产购置订单，新增购置信息，获得订单号
-"""
-Base.enter_estate_planing(3)
-Base.click_top_button(0)
-Base.add_order(code)
-time.sleep(1)
-Base.driver.find_element_by_xpath("/html/body/main/div/div/div/section/div/div/div/div[3]/table/thead/tr/th[2]").click()
-time.sleep(1)
-
-Base.choose_one_line(order_number)
-Base.click_top_button(2)
-upload_file_path = "D:\\Fixedtest\\FixedAssertTest\\FixedAssertTest\\AssetsPurchase\\data\\1.jpeg"
-Base.fill_asset_acquisition_detail(upload_file_path)
-
-"""
-申请验收
-"""
-Base.choose_one_line(order_number)
-Base.click_top_button(4)
-time.sleep(1)
-Base.driver.find_element_by_class_name("btn-primary").click()
